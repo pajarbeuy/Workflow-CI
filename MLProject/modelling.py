0efc0a39553595ed5311046323677d089e4ed078
pajarbeuy <pajar280505@gmail.com>
2026-06-02 03:07:58 +0700

    fix coding

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,9 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-tracking_path = "sqlite:///" + str(Path(__file__).parent / "mlruns.db")
-mlflow.set_tracking_uri(tracking_path)
-mlflow.set_experiment("Teen_Mental_Health")
 
 df = pd.read_csv("teen_social_media_preprocessed.csv")
 X = df.drop("depression_label", axis=1)
